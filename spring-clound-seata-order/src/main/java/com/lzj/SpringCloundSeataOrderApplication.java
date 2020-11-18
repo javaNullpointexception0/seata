b8cd7bf9d8b20dcc120ca2bd07e0f25024865867
luzhenjiang <luzhenjiang1991@126.com>
2020-11-18 23:02:31 +0800

    提交订单项目

---
+++ spring-clound-seata-order/src/main/java/com/lzj/SpringCloundSeataOrderApplication.java
@@ -0,0 +1,13 @@
+package com.lzj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloundSeataOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloundSeataOrderApplication.class, args);
+    }
+
+}
